e492a92496bbc888d5672f3a488206174e7783e5
komiya <90894519+misya11p@users.noreply.github.com>
2025-11-05 22:44:46 +0900

    format: entrypoint.shのインデントを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 cd "$(dirname "$0")" || exit 1
 
 case "$1" in
-    run)
-        docker compose up -d
-        ;;
-    stop)
-        docker compose down
-        ;;
-    *)
-        echo "Usage: $0 {run|stop}"
-        exit 1
-        ;;
+  run)
+    docker compose up -d
+    ;;
+  stop)
+    docker compose down
+    ;;
+  *)
+    echo "Usage: $0 {run|stop}"
+    exit 1
+    ;;
 esac
